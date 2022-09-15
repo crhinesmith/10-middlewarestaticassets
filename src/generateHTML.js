9b6cf62772dc7b29d771550f8358b72e6abaeb87
ripbog <crhinesmith5@gmail.com>
2022-09-14 21:26:32 -0400

    barely any styling added

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -1,7 +1,7 @@
 // this is similar to generateMarkdown file from homework 1 
 function generateHTML(teamMembers) {
     console.log(teamMembers)
-    let strCards = "<html lang='en'><head><meta charset='UTF-8' /><meta http-equiv='X-UA-Compatible'content='IE=edge' /> <meta name='viewport' content='width=device-width, initial-scale=1.0' /><title>MiddlewareStaticAssets</title><link rel='stylesheet' href='./styles/index.css' /></head><header>My Team</header>"
+    let strCards = "<html lang='en'><head><meta charset='UTF-8' /><meta http-equiv='X-UA-Compatible'content='IE=edge' /> <meta name='viewport' content='width=device-width, initial-scale=1.0' /><title>MiddlewareStaticAssets</title><link rel='stylesheet' href='./style.css' /></head><header>My Team</header>"
     //add the css link, add the HTML boilerplate, close the HTML tag at the end where div is c
     teamMembers.forEach((employee) => {
         strCards = strCards.concat(`
